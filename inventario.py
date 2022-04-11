8ecd9da82e3df5313cc8159970e0213985c58bea
XxCesarinxX <cesar1000sk@gmail.com>
2022-04-10 23:47:10 -0500

    segundo commit

--- inventario.py
+++ inventario.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df=pd.read_csv("lista_de_precios.csv")
 df2=pd.read_csv("inventa.csv")
+vent=pd.read_csv("ventas.csv")
 """
 def agregar_ainventario():
     agregar=input("agrega; en este orden\n->'Modelo'<-\t->'Color'<-\t'Tallas(separadas por espacios)'\n")
@@ -64,6 +65,8 @@ class venta:
         self.colo=color
         self.talla=talla
         self.total=precio
+        self.canti=1
+
     def decrementacion(self):
         for i in range(len(df2["Modelo"])):
             if self.model == df2.loc[i,"Modelo"]:
@@ -72,10 +75,27 @@ class venta:
     def ganancia(self):
         for i in range(len(df["Modelo"])):
             if self.model == df.loc[i,"Modelo"]:
-                costo =self.total- df.loc[i, "Precio"]
+                global costo, ganar
+                costo = df.loc[i, "Precio"]
+                ganar =  self.total - costo
                 #cosoto=self.total-costo
-        print("usted tiene una ganancia de: --> ", costo)
+        print("usted tiene una ganancia de: --> ",ganar)
+    def adjuntar(self):
+        ven=vent.append({'Cantidad':self.canti,'Modelo':self.model, 'Color':self.colo,'Talla':self.talla, 'Ganancia':ganar,'Total':self.total, 'Precio':costo}, ignore_index=True)
+        print(ven)
+        ven.to_csv("ventas.csv", index=False)
+    def reponer(self):
+        for i in range(len(vent["Modelo"])):
+            if self.model == vent.loc[i,"Modelo"]:
+                print('mismo modelo')
+                if self.colo == vent.loc[i,"Color"]:
+                    print('mismo color')
+                    if self.talla == vent.loc[i,"Talla"]:
+                        print('mismo talla')
+                        vent.loc[i,"Cantidad"]+=1
+            
+        print(vent)
+            
 
 dia = venta("Canelo", "NEGRO", "XL", 1500)
-dia.decrementacion()
-dia.ganancia()
+dia.reponer()
